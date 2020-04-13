caafe45097b2bb31b53e4a7116f146a121d3f936
vkWeb <vivekmittalagrawal@gmail.com>
2020-04-13 18:00:56 +0530

    fix: seg fault

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 /*************************
 File: main.c
 Author: Vivek Agrawal
-Purpose: Main program file
+Purpose: Main program file for solving 9x9 sudoku puzzles using backtracking algorithm.
 *************************/
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "constraints/constraints.h"
 
+struct Game
+{
+  int board[81];
+  int emptyCellIndices[81];
+  int emptyCellIndicesArrSize;
+};
+
 // Solve sudoku by using backtracking
-bool solveSudokuTillPos(int board[], int emptyCellIndex, int emptyCellPos[])
+bool solveSudokuTillPos(int board[], int emptyCellIndices[], int emptyCellIndex)
 {
   bool isSolvable = true;
-  int currentBoardPos = emptyCellPos[emptyCellIndex];
+  int currentBoardPos = emptyCellIndices[emptyCellIndex];
 
   while (board[currentBoardPos] <= 9 && isSolvable == true)
   {
@@ -24,7 +32,7 @@ bool solveSudokuTillPos(int board[], int emptyCellIndex, int emptyCellPos[])
       if (emptyCellIndex == 0)
         isSolvable = false;
       else
-        isSolvable = solveSudoku(board, currentBoardPos);
+        isSolvable = solveSudokuTillPos(board, emptyCellIndices, emptyCellIndex - 1);
     }
 
     else if (isValueLegal(board, currentBoardPos))
@@ -40,20 +48,35 @@ bool solveSudokuTillPos(int board[], int emptyCellIndex, int emptyCellPos[])
 // Main function
 int main(void)
 {
-  int sudokuBoard[81] = {5, 3, 0, 0, 7, 0, 0, 0, 0, 6, 0, 0, 1, 9, 5, 0, 0, 0, 0, 9, 8, 0, 0, 0, 0, 6, 0, 8, 0, 0, 0, 6, 0, 0, 0, 3, 4, 0, 0, 8, 0, 3, 0, 0, 1, 7, 0, 0, 0, 2, 0, 0, 0, 6, 0, 6, 0, 0, 0, 0, 2, 8, 0, 0, 0, 0, 4, 1, 9, 0, 0, 5, 0, 0, 0, 0, 8, 0, 0, 7, 9};
-  int emptyCellPos[81];
-  for (short int i = 0, j = 0; i < 81; ++i)
+  struct Game game;
+  int board[81] = {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 3, 0, 7, 4, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 2, 0, 8, 0, 0, 4, 0, 0, 1, 0, 6, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 7, 8, 0, 5, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0};
+
+  memcpy(game.board, board, sizeof(game.board));
+  //memcpy(game.emptyCellIndices,, sizeof(game.board));
+  game.emptyCellIndicesArrSize = 0;
+
+  for (short int i = 0; i < 81; ++i)
   {
-    if (sudokuBoard[i] == 0)
+    if (game.board[i] == 0)
     {
-      emptyCellPos[j] = i;
-      ++j;
+      game.emptyCellIndices[game.emptyCellIndicesArrSize] = i;
+      ++game.emptyCellIndicesArrSize;
     }
   }
 
   int i = 0;
-  while (solveSudokuTillPos(sudokuBoard, i, emptyCellPos))
+  while (i != game.emptyCellIndicesArrSize && solveSudokuTillPos(game.board, game.emptyCellIndices, i))
     ++i;
+
+  if (i != game.emptyCellIndicesArrSize)
+    return -1;
+
+  for (short int i = 0; i < 81; ++i)
+  {
+    if (i % 9 == 0)
+      printf("\n");
+    printf("%d ", game.board[i]);
+  }
 
   return 0;
 }
